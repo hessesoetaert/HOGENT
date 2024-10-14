162a78fcd4a5c8cada851b1073e12a06c20e5bfc
Hesse <hessesoetaert@hotmail.com>
2024-10-14 12:27:17 +0200

    vault backup: 2024-10-14 12:27:16

--- PROGRAMMING_BASIS/00/ConsoleApp2/ConsoleApp2/Program.cs
+++ PROGRAMMING_BASIS/00/ConsoleApp2/ConsoleApp2/Program.cs
@@ -3,87 +3,27 @@ using System.Reflection.Metadata;
 using System.Security.Principal;
 using System.Diagnostics;
 using System.Drawing;
-using System.Security.Cryptography.X509Certificates;
+using System.Collections;
 
 namespace ConsoleApp2
 {
     internal class Program
     {
-        public static string g_elapsedMSec = " START";
-        public static Stopwatch g_runTime = new Stopwatch();
-        public static int g_frameNr = 0;
-        
-
         static void Main(string[] args)
         {
-
-            bool runGame = true;
-            g_runTime.Start();
-
-
-
-
-
-
-
-
-            ConsoleSpiner spin = new ConsoleSpiner();
-            Console.Write("Working....\n");
-            double elapsedTime = 0;
-            while (runGame)
-            {
-                elapsedTime += Program.GetElapsedSec();
-                //if ()
-                {
-                    spin.Turn();
-                }
-
-
-                //ELAPSED SEC
-                g_runTime.Stop();
-                g_elapsedMSec = g_runTime.ElapsedMilliseconds.ToString();
-                g_runTime.Restart();
-            }
-        }
-
-        
-        public void GameLoop()
-        {
-
-        }
-
-        public void DrawFrame(double elapsedTime = 0)
-        {
-
-        }
-
-        double GetElapsedSec()
-        {
-           double elapsedSec = double.Parse(g_elapsedMSec) * 1000;
-            
-            return elapsedSec ;
-        }
-
-
-        public class ConsoleSpiner
-        {
-            int counter;
-            public ConsoleSpiner()
-            {
-                counter = 0;
-            }
-            public void Turn()
-            {
-                counter++;
-                switch (counter % 4)
-                {
-                    case 0: Console.Write("/"); break;
-                    case 1: Console.Write("-"); break;
-                    case 2: Console.Write("\\"); break;
-                    case 3: Console.Write("|"); break;
-                }
-                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
-            }
+            Console.WindowHeight = 20;
+            Console.WindowWidth = 20;
+            Console.WriteLine("Test 1");
+            Console.WriteLine("Max height: " + Console.LargestWindowHeight.ToString());
+            Console.WriteLine("Max width: " + Console.LargestWindowWidth.ToString());
+            Console.ReadKey();
+            Console.Clear();
+            Console.SetWindowSize(10, 10);
+            Console.WriteLine("Test 2");
+            Console.WriteLine("Max height: " + Console.LargestWindowHeight.ToString());
+            Console.WriteLine("Max width: " + Console.LargestWindowWidth.ToString());
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 }
--- PROGRAMMING_BASIS/00/ConsoleApp2/ConsoleApp2/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
+++ PROGRAMMING_BASIS/00/ConsoleApp2/ConsoleApp2/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cabf7efb37870d9b20c658458e8da12d29c76b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5914b2ad8ddbdbf70c10b0064463af9aacf3061")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
